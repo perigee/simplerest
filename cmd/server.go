6ed4822297844e167692452f68e62a9caf9134bb
Jun HU <jun.hu@ubisoft.com>
2016-10-29 12:25:31 -0700

    init server file

---
+++ cmd/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+       "log"
+       "net"
+
+       "golang.org/x/net/context"
+       "google.golang.org/grpc"
+       pb "github.com/perigee/terrant/pb/service"
+       "github.com/hashicorp/terraform/config"
+       "github.com/hashicorp/terraform/terraform"
+)
+
+const (
+      port = ":50051"
+)
+
+
+
+type server struct {}
+
+
+
+
+
+func (s *server) Destroy(ctx context.Context, in *pd.ApplyReq) (*pb.ApplyResp, error) {
+
+     
+     
+     return &pb.ApplyResp{Message: "Applying " + in.ProjectId}, nil
+}
+
+
+func (s *server) Apply(ctx context.Context, in *pd.ApplyReq) (*pb.ApplyResp, error) {
+
+   
+     
+     return &pb.ApplyResp{Message: "Applying " + in.ProjectId}, nil
+}
+
+
+
+func main() {
+     lis, err := net.Listen("tcp", port)
+     if err != nil {
+     	log.Fatalf("failed to listen: %v", err)
+     }
+
+     s := grpc.NewServer()
+     pb.RegisterProvisionServer(s, &server{})
+     if err := s.Serve(lis); err != nil {
+     	log.Fatalf("failed to serve: %v", err)
+     }
+}
