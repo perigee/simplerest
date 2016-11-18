715b706facd95efbaaa8a8e14a4825cc63da6ab8
Jun HU <junhufr@gmail.com>
2016-11-17 20:22:43 -0500

    init design

---
+++ design/design.go
@@ -0,0 +1,31 @@
+package design
+
+import (
+        . "github.com/goadesign/goa/design"
+        . "github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = API("infra", func() {
+        Title("The adder API")
+        Description("A teaser for goa")
+        Host("localhost:8080")
+        Scheme("http")
+	BasePath("/infra")
+	Consumes("application/json")
+	Produces("application/json")
+})
+
+var _ = Resource("resource", func() {
+	Action("create", func() {
+		Routing(POST("/resource/:resourceID"))
+		Description("create resource by its ID")
+		Params(func() {
+			Param("resourceID", String, "Resource ID")
+		})
+		Response("OK", func() {
+			Description("Return in case of creation")
+			Status(201)
+		})
+	})
+})
+
