5d7c674e5588b912b176e2737df287fd1dcc3252
perigee <junhufr@gmail.com>
2016-11-20 17:46:29 -0500

    init hcl mapping

---
+++ hclmapping.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+        "io/ioutil"
+	"github.com/hashicorp/hcl"
+	"github.com/hashicorp/hcl/hcl/ast"
+)
+
+
+func GetVaraiblesList(file *ast.File) ([]string, error) {
+     a := []string{"sef", "sdfe"}
+
+     return a, nil
+}
+
+func HCLmapper(filename string) (*ast.File, error) {
+      byteArray, err := ioutil.ReadFile(filename)
+
+      if err != nil {
+      	 return nil, err
+      }
+
+      hclAstFile, err := hcl.ParseBytes(byteArray)
+
+      if err != nil {
+      	 return nil, err
+      }
+
+      return hclAstFile, nil
+}
+
