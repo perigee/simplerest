e423663f414bcb0f67d7833220fecf0f29b6a517
Jun HU <jun.hu@ubisoft.com>
2016-11-23 01:15:45 +0000

    init op terra for terraform file manipulation

---
+++ opterra.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	TerraformState = "infra.tf"
+)
+
+// UpdateTerraFile updates:
+// 1. runlist
+// 2. trigger_hash
+func UpdateTerraFile(runlist string) error {
+
+	return nil
+}
