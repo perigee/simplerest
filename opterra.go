59a511508162ef0ef129d7a54b9ae6e36b223486
Jun HU <jun.hu@ubisoft.com>
2016-11-23 20:02:16 +0000

    func for s3 key generation, using constant instead

--- opterra.go
+++ opterra.go
@@ -27,14 +27,16 @@ import (
 )
 
 const (
-	BUCKETNAME     = "autoterrarepostate"
-	DEFAULTREGION  = "us-east-1"
-	TerraformState = "infra.tf"
+	terraBUCKETNAME     = "autoterrarepostate"
+	terraDEFAULTREGION  = "us-east-1"
+	terraTerraformState = "infra.tf"
+	terraStatusFile     = "resource.json"
+	terraAttributeFile  = "attributes.json"
 )
 
 func downloadS3object(s3client *s3.S3, key string) ([]byte, error) {
 	res, err := s3client.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String("autoterrarepostate"),
+		Bucket: aws.String(terraBUCKETNAME),
 		Key:    aws.String(key),
 	})
 
@@ -54,36 +56,60 @@ func downloadS3object(s3client *s3.S3, key string) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// FetchObject fetch the object
-func FetchObject(ctx *app.CreateChefContext) ([]byte, error) {
+func uploadS3object(s3client *s3.S3, key string, body []byte) error {
 
-	//
+	_, err := s3client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(terraBUCKETNAME),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(body),
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func s3KeyGen(ctx *app.CreateChefContext, filename string) string {
 	okey := make([]string, 3)
 
 	okey[0] = ctx.Request.Header.Get(design.SPACEID)
 	okey[1] = ctx.Payload.Vmuid
-	okey[2] = "infra.tf"
+	okey[2] = filename
+	return strings.Join(okey, "/")
+}
 
-	objectkey := strings.Join(okey, "/")
-
+// FetchObject fetch the object
+func FetchObject(ctx *app.CreateChefContext) ([]byte, error) {
 	sess, err := session.NewSession()
 
 	if err != nil {
 		panic(err)
 	}
 
-	svc := s3.New(sess, &aws.Config{Region: aws.String("us-east-1")})
+	svc := s3.New(sess, &aws.Config{Region: aws.String(terraDEFAULTREGION)})
 
-	resp, err := downloadS3object(svc, objectkey)
+	resp, err := downloadS3object(svc, s3KeyGen(ctx, terraStatusFile))
 
 	if err != nil {
 		fmt.Println(err.Error())
 		return nil, err
 	}
 
-	fmt.Println(resp)
+	if err := uploadS3object(svc, s3KeyGen(ctx, "jun_tmp_file"), resp); err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
 
 	return resp, nil
+}
+
+// CheckJobStatus verifies the status of current resource
+func CheckJobStatus(ctx *app.CreateChefContext) (string, error) {
+
+	return "", nil
 }
 
 // UpdateTerraFile updates
@@ -94,7 +120,6 @@ func UpdateTerraFile(s3obj *s3.GetObjectOutput) (*s3.GetObjectOutput, error) {
 // UploadObject upload the object on s3
 func UploadObject() error {
 	return nil
-
 }
 
 // JSON2File creates the file based on given json
